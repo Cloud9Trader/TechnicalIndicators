e928b88ac572b01a6fe38e84b6d3375bcb45899c
Jon Kafton <jonkafton@gmail.com>
2014-08-13 01:28:51 -0400

    Remove array shift optimization for simplicity

--- sma.src.js
+++ sma.src.js
@@ -1,8 +1,32 @@
+/* 
+ * This is the code for the classic Simple Moving Average indicator.
+ *
+ * There are plenty of indicators already built in and writing your own is fairly
+ * straightforward. Technical indicators can be drawn as price charts overlays or
+ * beneath them as studies. They can also be used inside your algorithms by calling
+ * the key specified to the right as a function, passing in any parameters,
+ * e.g. SMA_Example(15)
+ *
+ * There's an onStart function below to validate the input parameter, but writing an
+ * indicator can be as simple as declaring an onIntervalClose function that returns
+ * the value of the indicator.
+ *
+ * Indicators can return any number of values along with chart configuration to so
+ * they display exactly as you want them.
+ *
+ * See the documentation for more info:
+ * https://www.cloud9trader.com/documentation/writing-your-technical-indicators
+ *
+ * We've also open sourced the code for our built in indicators on GitHub.
+ * https://github.com/cloud9trader/technicalindicators
+ */
+
+
 var values = [];
-var length = null;
-var cacheSum = null;
 
 function getRunUpCount () {
+
+    // This tells the system how much run up price data to load prior to start date
     return periods;
 }
 
@@ -32,26 +56,25 @@ function onStart (periods) {
 // BID and ASK are close prices
 function onIntervalClose (periods) {
 
-    // Push closing mid price to end of values array
-    values.push(CLOSE);
+    var mid = (BID + ASK) / 2;
 
-    // Store length (slightly faster than accessing repeatedly)
-    length = values.length;
+    // Push closing mid price to the values array
+    values.push(mid);
 
-    // We haven't got enough run up data to produce a value, so return null
-    if (length < periods) {
+    // If we don't have enough run up yet
+    if (values.length < periods) {
+
+        // Retuning null indicates we cannot yet produce a value
         return null;
+
+    // If we have too many values, shift out the oldest (once run up data is filled, will always be one too many)
+    } else if (values.length > periods) {
+
+        // Shift out the oldest close price
+        values.shift();
     }
 
-    // Re-indexing array is slow, so rather than splicing dropped value from the beginning each time we just splice out old data every so often
-    if (length > periods * 10) {
-        values.splice(0, length - periods);
-        length = values.length;
-    }
-
-    // Clear the onTick cacheSum optimization
-    cacheSum = null;
 
     // Return the mean average
-    return Math.average(values.slice(values.length - periods));
+    return Math.average(values);
 }
